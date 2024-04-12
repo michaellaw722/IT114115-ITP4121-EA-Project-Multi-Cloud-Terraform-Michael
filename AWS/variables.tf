cac4bb08c6f2e9dcc0167b87bcf9209cd3f23c9b
michaellaw1124 <113016117+michaellaw1124@users.noreply.github.com>
2024-04-12 13:03:03 +0800

    Add aws mian and var

---
+++ AWS/variables.tf
@@ -0,0 +1,26 @@
+variable "region" {
+    description = "The AWS region where resources will be provisioned"
+    type        = string
+    default     = "us-west-2"
+}
+
+variable "subnet_id" {
+    description = "The ID of the subnet where the EC2 instance will be launched"
+    type        = string
+}
+
+variable "vpc_id" {
+    description = "The ID of the VPC where the EC2 instance will be launched"
+    type        = string
+}
+
+variable "region" {
+    description = "The AWS region where resources will be provisioned"
+    type        = string
+    default     = "us-west-2"
+}
+
+variable "subnet_id" {
+    description = "The ID of the subnet where the EC2 instance will be launched"
+    type        = string
+}
