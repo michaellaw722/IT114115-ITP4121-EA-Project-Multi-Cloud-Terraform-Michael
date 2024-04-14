a3edf3d23064c479785f1b44cfe284f0f7970cce
michaellaw1124 <113016117+michaellaw1124@users.noreply.github.com>
2024-04-14 09:12:54 +0000

    Update var

--- AWS/variables.tf
+++ AWS/variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
     description = "The AWS region where resources will be provisioned"
     type        = string
-    default     = "us-west-2"
+    default     = "ap-east-1"
 }
 
 variable "subnet_id" {
@@ -11,16 +11,5 @@ variable "subnet_id" {
 
 variable "vpc_id" {
     description = "The ID of the VPC where the EC2 instance will be launched"
-    type        = string
-}
-
-variable "region" {
-    description = "The AWS region where resources will be provisioned"
-    type        = string
-    default     = "us-west-2"
-}
-
-variable "subnet_id" {
-    description = "The ID of the subnet where the EC2 instance will be launched"
     type        = string
 }
