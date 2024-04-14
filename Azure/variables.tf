c06192e8beeebb9785a02fe10f33648c35276903
michaellaw1124 <113016117+michaellaw1124@users.noreply.github.com>
2024-04-14 08:08:18 +0000

    add var

--- Azure/variables.tf
+++ Azure/variables.tf
@@ -1,3 +1,11 @@
-variable "var_project" {
-        default = "project-name"
-    }
+variable "subscription_id" {
+    description = "project-name"
+}
+
+variable "app_id" {
+    description = "K8s Service"
+}
+
+variable "password" {
+    description = "K8s password"
+}
