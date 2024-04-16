a6865a7e795df9fb9d25810a37d3c1d7b23edd62
michaellaw1124 <113016117+michaellaw1124@users.noreply.github.com>
2024-04-16 09:46:14 +0000

    add nsg

--- Azure/main.tf
+++ Azure/main.tf
@@ -56,3 +56,14 @@ resource "azurerm_subnet" "Public_Subnet2" {
     "192.168.4.0/24",
   ]
 }
+
+
+resource "azurerm_network_security_group" "aks_nsg" {
+  name                = "aks-nsg"
+  location            = azurerm_virtual_network.CloudProjectVPC.location
+  resource_group_name = azurerm_resource_group.CloudProjectGroup.name
+
+  tags = {
+    Name = "AKS NSG"
+  }
+}
