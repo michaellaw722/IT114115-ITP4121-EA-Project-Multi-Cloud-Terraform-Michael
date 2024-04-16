46d5c53b252caaae14d626457f37d378327ba6de
michaellaw1124 <113016117+michaellaw1124@users.noreply.github.com>
2024-04-16 07:04:00 +0000

    Add public Subnet

--- GCP/main.tf
+++ GCP/main.tf
@@ -1,19 +1,33 @@
-resource "google_compute_network" "vpc_network" {
-  name                    = "CloudProject-network"
+resource "google_compute_network" "CloudProject-network" {
+  name                    = "cloudnetwork"
   auto_create_subnetworks = false
   mtu                     = 1460
 }
 
 resource "google_compute_subnetwork" "PrivateSubnet1" {
-  name          = "my-custom-subnet"
+  name          = "privatesubnet1"
   ip_cidr_range = "10.0.1.0/24"
+  region        = "asia-east2"
+  network       = google_compute_network.CloudProject-network.id
+}
+
+resource "google_compute_subnetwork" "PrivateSubnet2" {
+  name          = "privatesubnet2"
+  ip_cidr_range = "10.0.2.0/24"
+  region        = "asia-east2"
+  network       = google_compute_network.CloudProject-network.id
+}
+
+resource "google_compute_subnetwork" "PublicSubnet1" {
+  name          = "publicsubnet1"
+  ip_cidr_range = "10.0.3.0/24"
   region        = "asia-east2"
   network       = google_compute_network.vpc_network.id
 }
 
-resource "google_compute_subnetwork" "PrivateSubnet2" {
-  name          = "my-custom-subnet"
-  ip_cidr_range = "10.0.2.0/24"
+resource "google_compute_subnetwork" "PublicSubnet2" {
+  name          = "publicsubnet2"
+  ip_cidr_range = "10.0.4.0/24"
   region        = "asia-east2"
   network       = google_compute_network.vpc_network.id
 }
