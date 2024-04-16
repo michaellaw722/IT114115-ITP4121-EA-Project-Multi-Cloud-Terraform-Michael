93931dfa3c47292a3dbb43ed55efe8ee9349eb41
michaellaw1124 <113016117+michaellaw1124@users.noreply.github.com>
2024-04-16 06:46:13 +0000

    add vpc and subnet

--- AWS/main.tf
+++ AWS/main.tf
@@ -1,27 +1,47 @@
-resource "aws_vpc" "Cloud_Project" {
+resource "aws_vpc" "EKSProject" {
   cidr_block = "172.0.0.0/16"
+  
+  tags = {
+    Name = "EKSProject"
+  }
 }
 
 resource "aws_subnet" "Public_Subnet1a" {
-  vpc_id            = aws_vpc.Cloud_Project.id
+  vpc_id            = aws_vpc.EKSProject.id
   cidr_block        = "172.0.1.0/24"
   availability_zone = "ap-east-1a"
+  
+  tags = {
+    Name = "Public_Subnet1a"
+  }
 }
 
 resource "aws_subnet" "Public_Subnet1b" {
-  vpc_id            = aws_vpc.Cloud_Project.id
+  vpc_id            = aws_vpc.EKSProject.id
   cidr_block        = "172.0.2.0/24"
   availability_zone = "ap-east-1b"
+  
+  tags = {
+    Name = "Public_Subnet1b"
+  }
 }
 
 resource "aws_subnet" "Private_Subnet1a" {
-  vpc_id            = aws_vpc.Cloud_Project.id
+  vpc_id            = aws_vpc.EKSProject.id
   cidr_block        = "172.0.3.0/24"
   availability_zone = "ap-east-1a"
+  
+  tags = {
+    Name = "Private_Subnet1a"
+  }
 }
 
 resource "aws_subnet" "Private_Subnet1b" {
-  vpc_id            = aws_vpc.Cloud_Project.id
+  vpc_id            = aws_vpc.EKSProject.id
   cidr_block        = "172.0.4.0/24"
   availability_zone = "ap-east-1b"
+  
+  tags = {
+    Name = "Private_Subnet1b"
+  }
 }
